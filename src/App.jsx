ada720832f0b1ef98b28508c1be329e81baeffcd
sergioedo <sergio.edo@gmail.com>
2023-06-25 17:29:54 +0200

    index html template to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="container">
+      <article className="no-padding primary-container responsive round">
+        <img
+          id="audio-image"
+          className="primary responsive small top-round"
+          src="img/abuela-cascos.svg"
+          style={{objectFit: 'contain'}}
+        />
+        <div className="padding bottom-round white">
+          <h5 id="audio-title">Play Well Abuela</h5>
+          <p className="left-align" id="audio-description">
+            El reproductor de audio que hasta tu abuela sabrá utilizar!
+            Selecciona un audio y dale al play!
+          </p>
+          <nav className="center-align">
+            <audio
+              id="audio-player"
+              src=""
+              controls
+              preload="auto"
+              style={{ width: '80%' }}
+            ></audio>
+            <button
+              // onClick="changePlayBackRate()"
+              id="playback-rate-btn"
+              className="circle large medium-elevate"
+            >
+              1x
+            </button>
+          </nav>
+        </div>
+      </article>
+      <article className="primary responsive">
+        <h6 className="small bold center-align no-margin">
+          Últimos episodios de Web Reactiva
+        </h6>
+      </article>
+      <article className="tertiary responsive" id="episodes-list"></article>
+    </main>
   )
 }
 
